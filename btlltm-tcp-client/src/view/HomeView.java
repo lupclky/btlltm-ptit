5e92aa37e591ba63571a73412d8eaa03a6d93262
dangnguyen2311 <nguyenquanghaidang2311@gmail.com>
2024-11-14 23:01:01 +0700

    Sửa nốt giao diện nhắn tin

--- btlltm-tcp-client/src/view/HomeView.java
+++ btlltm-tcp-client/src/view/HomeView.java
@@ -200,7 +200,7 @@ public class HomeView extends javax.swing.JFrame {
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, 60, 60));
 
         btnExit.setBackground(new java.awt.Color(153, 0, 0));
-        btnExit.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        btnExit.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         btnExit.setForeground(new java.awt.Color(204, 255, 255));
         btnExit.setText("EXIT");
         btnExit.addActionListener(new java.awt.event.ActionListener() {
